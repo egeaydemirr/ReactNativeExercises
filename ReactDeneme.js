c719c3e9702f48c1b82f82021f2a0cb27b0bc17b
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-21 11:17:37 +0300

    Hooks

---
+++ ReactDeneme.js
@@ -0,0 +1,2 @@
+// React Hooks Nedir?
+// Hook ne demek ? Kanca. Kodda bir yerlere kanca atmak. Yani ortak bir takım fonksiyonlara kanca atarak bileşenimizi güç ve yetenek kazandırmak anlamına geliyor. Nasıl güç kazandığına yazının devamından okuyabilirsiniz.
