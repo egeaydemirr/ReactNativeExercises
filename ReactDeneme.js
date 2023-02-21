df533143a5f1937d8962db53b27f1767a7edb13e
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-21 11:20:52 +0300

    useState()

--- ReactDeneme.js
+++ ReactDeneme.js
@@ -1,2 +1,20 @@
 // React Hooks Nedir?
 // Hook ne demek ? Kanca. Kodda bir yerlere kanca atmak. Yani ortak bir takım fonksiyonlara kanca atarak bileşenimizi güç ve yetenek kazandırmak anlamına geliyor. Nasıl güç kazandığına yazının devamından okuyabilirsiniz.
+
+
+
+
+ // USESTATE()
+
+// const Set: NextPage = () => {
+
+//   const [baslangic, setBaslangic] = useState(0)
+//   return (
+//     <>
+//     <div>
+//       <button onClick= {() => setBaslangic(baslangic + 1 ) }> Button </button>
+//       <h1>baslangic: {baslangic}</h1>
+//     </div>
+//     </>
+//   )
+// }
