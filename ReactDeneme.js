7b1a24a72dfb99bc6a3248a58c143c862ca56718
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-24 15:29:15 +0300

    Render with an If-Else Condition

--- ReactDeneme.js
+++ ReactDeneme.js
@@ -491,3 +491,41 @@
 //   }
 // }
 // 
+
+
+
+
+// Render with an If-Else Condition
+class MyComponent extends React.Component{
+  constructor(props){
+    super(props); 
+    this.state = {
+      display: true
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this)
+  }
+  toggleDisplay(){
+    this.setState(state =>({
+      display: !state.display
+    }))
+  }
+  render(){
+    if(this.state.display === true){
+      return(
+        <div>
+          <button onClick={this.toggleDisplay}>Toggle Display</button>
+          <h1>Displayed</h1>
+        </div>
+      )
+    }
+    else{
+      return(
+        <div>
+          <button onClick={this.toggleDisplay}>Toggle Display</button>
+        </div>
+      )
+    }
+ 
+  }
+}
+// :Render with an If-Else Condition
