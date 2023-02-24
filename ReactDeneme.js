48d8d9592758b6ad0b746b2842924ea9efd1e2b1
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-24 16:15:33 +0300

    Use && for a More Concise Conditional

--- ReactDeneme.js
+++ ReactDeneme.js
@@ -495,37 +495,70 @@
 
 
 
-// Render with an If-Else Condition
-class MyComponent extends React.Component{
-  constructor(props){
-    super(props); 
-    this.state = {
-      display: true
-    }
-    this.toggleDisplay = this.toggleDisplay.bind(this)
-  }
-  toggleDisplay(){
-    this.setState(state =>({
-      display: !state.display
-    }))
-  }
-  render(){
-    if(this.state.display === true){
-      return(
-        <div>
-          <button onClick={this.toggleDisplay}>Toggle Display</button>
-          <h1>Displayed</h1>
-        </div>
-      )
-    }
-    else{
-      return(
-        <div>
-          <button onClick={this.toggleDisplay}>Toggle Display</button>
-        </div>
-      )
-    }
+// // Render with an If-Else Condition
+// class MyComponent extends React.Component{
+//   constructor(props){
+//     super(props); 
+//     this.state = {
+//       display: true
+//     }
+//     this.toggleDisplay = this.toggleDisplay.bind(this)
+//   }
+//   toggleDisplay(){
+//     this.setState(state =>({
+//       display: !state.display
+//     }))
+//   }
+//   render(){
+//     if(this.state.display === true){
+//       return(
+//         <div>
+//           <button onClick={this.toggleDisplay}>Toggle Display</button>
+//           <h1>Displayed</h1>
+//         </div>
+//       )
+//     }
+//     else{
+//       return(
+//         <div>
+//           <button onClick={this.toggleDisplay}>Toggle Display</button>
+//         </div>
+//       )
+//     }
  
-  }
-}
-// :Render with an If-Else Condition
+//   }
+// }
+// // :Render with an If-Else Condition
+
+
+// Use && for a More Concise Conditional
+
+// If you write a lot of else if statements to return slightly different UIs, you may repeat code which leaves room for error. 
+// Instead, you can use the && logical operator to perform conditional logic in a more concise way.
+//  This is possible because you want to check if a condition is true, and if it is, return some markup. Here's an example:
+
+// {condition && <p>markup</p>}
+
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       display: true
+//     }
+//     this.toggleDisplay = this.toggleDisplay.bind(this);
+//   }
+//   toggleDisplay() {
+//     this.setState(state => ({
+//       display: !state.display
+//     }));
+//   }
+//   render() {
+//     // Change code below this line
+//     return (
+//        <div>
+//          <button onClick={this.toggleDisplay}>Toggle Display</button>
+//          {this.state.display && <h1>Displayed!</h1>}
+//        </div>
+//     );
+//   }
+// };
