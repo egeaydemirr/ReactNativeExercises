48d73b82fb0660191ac9eea20696bb542578b1e2
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-24 14:45:34 +0300

    EventListener

--- LifecycleMethods.js
+++ LifecycleMethods.js
@@ -94,3 +94,53 @@
 // };
 
 // :Add Event Listeners
+
+
+// Optimize Re-Renders with shouldComponentUpdate
+// React provides a lifecycle method you can call when child components receive new state or props, and declare specifically if the components should update or not.
+// The method is shouldComponentUpdate(), and it takes nextProps and nextState as parameters.
+// This method is a useful way to optimize performance.
+
+// class OnlyEvens extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   shouldComponentUpdate(nextProps, nextState) {
+//     console.log('Should I update?');
+//     // Change code below this line
+//       if (nextProps.value % 2 == 0) {
+//         return true;
+//       }
+//       return false;
+//     // Change code above this line
+//   }
+//   componentDidUpdate() {
+//     console.log('Component re-rendered.');
+//   }
+//   render() {
+//     return <h1>{this.props.value}</h1>;
+//   }
+// }
+
+// class Controller extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       value: 0
+//     };
+//     this.addValue = this.addValue.bind(this);
+//   }
+//   addValue() {
+//     this.setState(state => ({
+//       value: state.value + 1
+//     }));
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.addValue}>Add</button>
+//         <OnlyEvens value={this.state.value} />
+//       </div>
+//     );
+//   }
+// }
