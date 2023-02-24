16329b34b28e69d42289dff40b89da1831459918
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-24 14:52:11 +0300

    Use Advanced JavaScript in React Render Method

--- ReactDeneme.js
+++ ReactDeneme.js
@@ -333,83 +333,161 @@
 //:// Create a Controlled Form
 
 
-//Pass a Callback as Props
-class MyApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: ''
-    }
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
-    this.setState({
-      inputValue: event.target.value
-    });
-  }
-  render() {
-    return (
-       <div>
-        { /* Change code below this line */ }
-        <GetInput 
-        input={this.state.inputValue}
-        handleChange={this.handleChange}
-        />
-        <RenderInput
-        input = {this.state.inputValue}
-        />
+// //Pass a Callback as Props
+// class MyApp extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       inputValue: ''
+//     }
+//     this.handleChange = this.handleChange.bind(this);
+//   }
+//   handleChange(event) {
+//     this.setState({
+//       inputValue: event.target.value
+//     });
+//   }
+//   render() {
+//     return (
+//        <div>
+//         { /* Change code below this line */ }
+//         <GetInput 
+//         input={this.state.inputValue}
+//         handleChange={this.handleChange}
+//         />
+//         <RenderInput
+//         input = {this.state.inputValue}
+//         />
         
 
-        { /* Change code above this line */ }
-       </div>
-    );
-  }
-};
+//         { /* Change code above this line */ }
+//        </div>
+//     );
+//   }
+// };
 
-class GetInput extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <h3>Get Input:</h3>
-        <input
-          value={this.props.input}
-          onChange={this.props.handleChange}/>
-      </div>
-    );
-  }
-};
+// class GetInput extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h3>Get Input:</h3>
+//         <input
+//           value={this.props.input}
+//           onChange={this.props.handleChange}/>
+//       </div>
+//     );
+//   }
+// };
 
-class RenderInput extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <h3>Input Render:</h3>
-        <p>{this.props.input}</p>
-      </div>
-    );
-  }
-};
-//:Pass a Callback as Props
+// class RenderInput extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h3>Input Render:</h3>
+//         <p>{this.props.input}</p>
+//       </div>
+//     );
+//   }
+// };
+// //:Pass a Callback as Props
 
 
 
-// Add Inline Styles in React
-const styles = {color: "purple", fontSize: 40, border: "2px solid purple"}
+// // Add Inline Styles in React
+// const styles = {color: "purple", fontSize: 40, border: "2px solid purple"}
 
-class Colorful extends React.Component {
-  render() {
-    // Change code below this line
-    return (
-      <div style={styles}>Style Me!</div>
-    );
-    // Change code above this line
-  }
-};
+// class Colorful extends React.Component {
+//   render() {
+//     // Change code below this line
+//     return (
+//       <div style={styles}>Style Me!</div>
+//     );
+//     // Change code above this line
+//   }
+// };
 
-// : Add Inline Styles in React
+// // : Add Inline Styles in React
+
+// Use Advanced JavaScript in React Render Method
+
+// const inputStyle = {
+//   width: 235,
+//   margin: 5
+// };
+
+
+// class MagicEightBall extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       userInput: '',
+//       randomIndex: ''
+//     };
+//     this.ask = this.ask.bind(this);
+//     this.handleChange = this.handleChange.bind(this);
+//   }
+//   ask() {
+//     if (this.state.userInput) {
+//       this.setState({
+//         randomIndex: Math.floor(Math.random() * 20),
+//         userInput: ''
+//       });
+//     }
+//   }
+//   handleChange(event) {
+//     this.setState({
+//       userInput: event.target.value
+//     });
+//   }
+//   render() {
+//     const possibleAnswers = [
+//       'It is certain',
+//       'It is decidedly so',
+//       'Without a doubt',
+//       'Yes, definitely',
+//       'You may rely on it',
+//       'As I see it, yes',
+//       'Outlook good',
+//       'Yes',
+//       'Signs point to yes',
+//       'Reply hazy try again',
+//       'Ask again later',
+//       'Better not tell you now',
+//       'Cannot predict now',
+//       'Concentrate and ask again',
+//       "Don't count on it",
+//       'My reply is no',
+//       'My sources say no',
+//       'Most likely',
+//       'Outlook not so good',
+//       'Very doubtful'
+//     ];
+//     const answer = possibleAnswers[this.state.randomIndex];; // Change this line
+//     return (
+//       <div>
+//         <input
+//           type='text'
+//           value={this.state.userInput}
+//           onChange={this.handleChange}
+//           style={inputStyle}
+//         />
+//         <br />
+//         <button onClick={this.ask}>Ask the Magic Eight Ball!</button>
+//         <br />
+//         <h3>Answer:</h3>
+//         <p>
+//           {/* Change code below this line */}
+// {answer}
+//           {/* Change code above this line */}
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+// 
